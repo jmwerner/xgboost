9b12ce70a783c7035329729eda11d17a8bcb81ab
Jeremy Werner <Jeremy.Werner@allstate.com>
2017-11-20 23:12:46 -0600

    Add extra precision to xgb client libs dump functions

--- src/cli_main.cc
+++ src/cli_main.cc
@@ -289,6 +289,7 @@ void CLIDumpModel(const CLIParam& param) {
   std::unique_ptr<dmlc::Stream> fo(
       dmlc::Stream::Create(param.name_dump.c_str(), "w"));
   dmlc::ostream os(fo.get());
+  os.precision(18);
   if (param.dump_format == "json") {
     os << "[" << std::endl;
     for (size_t i = 0; i < dump.size(); ++i) {
@@ -332,6 +333,7 @@ void CLIPredict(const CLIParam& param) {
   std::unique_ptr<dmlc::Stream> fo(
       dmlc::Stream::Create(param.name_pred.c_str(), "w"));
   dmlc::ostream os(fo.get());
+  os.precision(18);
   for (bst_float p : preds) {
     os << p << '\n';
   }
--- src/tree/tree_model.cc
+++ src/tree/tree_model.cc
@@ -5,6 +5,7 @@
  */
 #include <xgboost/tree_model.h>
 #include <sstream>
+#include <iomanip>
 #include "./param.h"
 
 namespace xgboost {
@@ -144,6 +145,7 @@ std::string RegTree::DumpModel(const FeatureMap& fmap,
                                bool with_stats,
                                std::string format) const {
   std::stringstream fo("");
+  fo.precision(18);
   for (int i = 0; i < param.num_roots; ++i) {
     DumpRegTree(fo, *this, fmap, i, 0, false, with_stats, format);
   }
